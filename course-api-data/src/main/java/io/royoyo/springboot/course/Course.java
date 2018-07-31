4b6cc6968943c30d650c0f2acdc682c2cfd8148a
Ranajoy Roy <40860781+rroyoyo@users.noreply.github.com>
2018-07-31 15:36:25 +0530

    updated course.java
    included column and table dependencies to link project with PostgreSQL and prefixed instance variables with required annotations

--- course-api-data/src/main/java/io/royoyo/springboot/course/Course.java
+++ course-api-data/src/main/java/io/royoyo/springboot/course/Course.java
@@ -3,19 +3,23 @@ package io.royoyo.springboot.course;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+import javax.persistence.Column;
+import javax.persistence.Table;
 import io.royoyo.springboot.topic.Topic;
 
 //@Entity annotation lets spring know that we want the data base 
 //to contain rows with these objects (topic instances)
 @Entity 
+@Table(name = "course")
 public class Course {
 
 	@Id
+	@Column(name = "cid")
 	private String id;
+	@Column(name = "cname")
 	private String name;
+	@Column(name = "cdesc")
 	private String description;
-	
 	@ManyToOne
 	private Topic topic;
 	//relationship betwwen course and topic many to one, hence the annotation
